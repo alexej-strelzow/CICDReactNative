a6a29d662af61bc0d35480d31bf0f686a0834e90
Alexej Strelzow <alexej.strelzow@platomics.com>
2020-12-24 19:47:01 +0100

    add feature 1

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, { useEffect } from 'react';
-import {StyleSheet, View, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Input, Button, Text} from 'react-native-elements';
 import Crashes from 'appcenter-crashes';
 import Analytics from 'appcenter-analytics';
 
 const App = () => {
+
+  const [state, setState] = useState({
+    inflationRate: 0,
+    riskFreeRate: 0,
+    amount: 0,
+    timeInYears: 1,
+    afterInflation: 0,
+    atRiskFree: 0,
+    atRiskFreeAfterInflation: 0,
+    difference: 0
+  });
+
+  const calculateInflationImpact = (value, inflationRate, time) => {
+    return value / Math.pow(1 + inflationRate, time);
+  };
+
+  const calculate = () => {
+    let afterInflation = calculateInflationImpact(state.amount, state.inflationRate / 100, state.timeInYears);
+    let atRiskFree = state.amount * Math.pow(1 + state.riskFreeRate / 100, state.timeInYears);
+    let atRiskFreeAfterInflation = calculateInflationImpact(atRiskFree, state.inflationRate / 100, state.timeInYears);
+    let difference = atRiskFreeAfterInflation - afterInflation;
+
+    setState({
+      ...state,
+      afterInflation,
+      atRiskFree,
+      atRiskFreeAfterInflation,
+      difference
+    });
+  };
+
   const checkPreviousSession = async () => {
     const didCrash = await Crashes.hasCrashedInLastSession();
     if (didCrash) {
@@ -24,17 +49,41 @@ const App = () => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Button title="Crash" onPress={() => Crashes.generateTestCrash() }/>
+      <View style={styles.container}>
+          <Text h1 style={{marginVertical: 15}}>Inflation Calc</Text>
+        <Input placeholder="Current inflation rate"
+               keyboardType='decimal-pad'
+               onChangeText={(inflationRate) => setState({...state, inflationRate})}/>
+        <Input placeholder="Current risk free rate"
+               keyboardType='decimal-pad'
+               onChangeText={(riskFreeRate) => setState({...state, riskFreeRate})}/>
+        <Input placeholder="Amount you want to save"
+               keyboardType='decimal-pad'
+               onChangeText={(amount) => setState({...state, amount})}/>
+        <Input placeholder="For how long (in years) will you save?"
+               keyboardType='decimal-pad'
+               onChangeText={(timeInYears) => setState({...state, timeInYears})}/>
 
-      <Button title="Calculate Inflation" onPress={() => Analytics.trackEvent('calculate_inflation', { Internet: 'WiFi', GPS: 'Off'}) }/>
-    </View>
+        <Button title="Calculate inflation"
+                style={{marginVertical: 15}}
+                onPress={() => {
+                  calculate();
+                  Analytics.trackEvent('calculate_inflation', {Internet: 'WiFi', GPS: 'Off'});
+                }}/>
+
+        <Text>{state.timeInYears} years from now you will still have ${state.amount} but it
+          will only be worth ${state.afterInflation}.</Text>
+        <Text>But if you invest it at a risk free rate you will have
+          ${state.atRiskFree}.</Text>
+        <Text>Which will be worth ${state.atRiskFreeAfterInflation} after inflation.</Text>
+        <Text>A difference of: ${state.difference}.</Text>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 25,
+    marginTop: 50,
   },
 });
 
